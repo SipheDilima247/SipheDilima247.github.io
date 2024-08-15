4aeff130619754818f97959630f8ceddd13b2170
Siphelelele Dilima <SipheleleDilima@hotmail.com>
2024-08-15 09:40:26 +0200

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -215,4 +215,17 @@ document.addEventListener('DOMContentLoaded', () => {
    }
   }
 
+  document.getElementById('toggle-chatbot').addEventListener('click', function()
+  {
+    var chatbot = document.getElementById('chatbot');
+    if (chatbot.style.display === 'none') {
+      chatbot.style.display = 'block';}
+
+    else {
+      chatbot.style.display = 'none';
+    }
+  });
+
+  
+  
 });
